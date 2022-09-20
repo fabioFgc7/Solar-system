a0c43257462af2b6193fbffe52fb2d6ab6749b44
fabioFgc7 <99999716+fabioFgc7@users.noreply.github.com>
2022-09-20 12:53:14 -0400

    Se modifico el ind4ex.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     extend: {
       animation: {
         'spin-slow': 'spin 100s linear infinite',
+      },
+      backgroundImage: {
+        'background-img': "url('./public/img/background.jpg')",
       }
     },
   },
